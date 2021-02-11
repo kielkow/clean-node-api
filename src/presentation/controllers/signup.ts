7045c96d7372ce8e893f00c129cfe7b6b2c07ab4
kielkow <matheuskiel@fiorifer.com.br>
2021-02-10 23:06:43 -0300

    feat: ensure SignUpController return an error if no password is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -13,5 +13,12 @@ export class SignUpController {
         body: new Error('Missing param: email')
       }
     }
+
+    if (!httpRequest.body.password) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: password')
+      }
+    }
   }
 }
