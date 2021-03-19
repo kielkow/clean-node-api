a033fc02504a6109ff5a3568e17bf18cf018e025
kielkow <matheuskiel@fiorifer.com.br>
2021-03-19 09:48:49 -0300

    test: ensure RequiredFieldValidation not return if validation succeeds

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -5,8 +5,16 @@ describe('Required Field Validation', () => {
   test('Should return a MissingParamError if validation fails', () => {
     const sut = new RequiredFieldValidation('field')
 
-    const error = sut.validate({})
+    const error = sut.validate({ name: 'any_name' })
 
     expect(error).toEqual(new MissingParamError('field'))
+  })
+
+  test('Should not return if validation succeeds', () => {
+    const sut = new RequiredFieldValidation('field')
+
+    const error = sut.validate({ field: 'any_name' })
+
+    expect(error).toBeFalsy()
   })
 })
