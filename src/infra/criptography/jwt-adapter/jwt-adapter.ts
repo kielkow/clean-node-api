e149cf01b2043aac9f571876fbb6d3ba707a3903
kielkow <matheuskiel@fiorifer.com.br>
2021-03-30 12:37:08 -0300

    feat: ensure JwtAdapter calls sign with correct values

---
+++ src/infra/criptography/jwt-adapter/jwt-adapter.ts
@@ -0,0 +1,16 @@
+import jwt from 'jsonwebtoken'
+import { Encrypter } from '../../../data/protocols/criptografy/encrypter'
+
+export class JwtAdapter implements Encrypter {
+  private readonly secret: string
+
+  constructor (secret: string) {
+    this.secret = secret
+  }
+
+  async encrypt (value: string): Promise<string> {
+    await jwt.sign({ id: value }, this.secret)
+
+    return null
+  }
+}
