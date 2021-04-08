844460386228562b614ade362dd68def6697b559
kielkow <matheuskiel@fiorifer.com.br>
2021-04-08 11:31:50 -0300

    fix: clean git cache

--- src/data/usecases/authentication/db-authentication-protocols.ts
+++
@@ -1,6 +0,0 @@
-export * from '../../../domain/models/account'
-export * from '../../../domain/usecases/authentication'
-export * from '../../protocols/db/account/load-account-by-email-repository'
-export * from '../../protocols/db/account/update-access-token-repository'
-export * from '../../protocols/criptografy/hash-comparer'
-export * from '../../protocols/criptografy/encrypter'
