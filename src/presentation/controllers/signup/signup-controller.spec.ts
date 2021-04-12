b92fd6fff215b81cf26e5cfb9ab6b421648625e6
kielkow <matheuskiel@fiorifer.com.br>
2021-04-12 19:50:07 -0300

    test: ensure SignUpController returns an accessToken on success

--- src/presentation/controllers/signup/signup-controller.spec.ts
+++ src/presentation/controllers/signup/signup-controller.spec.ts
@@ -104,7 +104,7 @@ describe('SignUp Controller', () => {
 
     const httpResponse = await sut.handle(makeFakeRequest())
 
-    expect(httpResponse).toEqual(ok(makeFakeAccount()))
+    expect(httpResponse).toEqual(ok({ accessToken: 'any_token' }))
   })
 
   test('Should call Validation with correct value', async () => {
