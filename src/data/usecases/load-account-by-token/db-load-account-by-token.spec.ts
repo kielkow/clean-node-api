a3d61cb5c1c86b8dad1e6cb37ff70ace01e9fd8b
kielkow <matheuskiel@fiorifer.com.br>
2021-04-25 18:19:26 -0300

    test: ensure DbLoadAccountByToken returns null if LoadAccountByTokenRepository returns null

--- src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
+++ src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
@@ -80,4 +80,16 @@ describe('DbLoadAccountByTokenm', () => {
 
     expect(loadByTokenSpy).toHaveBeenCalledWith('any_token', 'any_role')
   })
+
+  test('Should return null if LoadAccountByTokenRepository returns null', async () => {
+    const { sut, loadAccountByTokenRepositoryStub } = makeSut()
+
+    jest.spyOn(loadAccountByTokenRepositoryStub, 'loadByToken').mockReturnValueOnce(
+      new Promise(resolve => resolve(null))
+    )
+
+    const account = await sut.load('any_token', 'any_role')
+
+    expect(account).toBeNull()
+  })
 })
