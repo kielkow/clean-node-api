161af1a2d885e5cfa934ff96d3f2559a38837138
kielkow <matheuskiel@fiorifer.com.br>
2021-04-26 10:01:58 -0300

    feat: ensure JwtAdapter throws if verify throws

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -66,5 +66,17 @@ describe('JWT Adapter', () => {
 
       expect(value).toBe('any_value')
     })
+
+    test('Should throw if verify throws', async () => {
+      const sut = makeSut()
+
+      jest.spyOn(jwt, 'verify').mockImplementationOnce(() => {
+        throw new Error()
+      })
+
+      const promise = sut.decrypt('any_token')
+
+      await expect(promise).rejects.toThrow()
+    })
   })
 })
