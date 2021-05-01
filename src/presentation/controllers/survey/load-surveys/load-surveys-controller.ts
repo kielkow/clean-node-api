6883308449181ea58dd8d3baab17657e27dcc269
kielkow <matheuskiel@fiorifer.com.br>
2021-04-30 23:42:27 -0300

    feat: ensure LoadSurveysController returns 200 on success

--- src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
@@ -1,3 +1,4 @@
+import { ok } from '../../../helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse, LoadSurveys } from './load-surveys-controller-protocols'
 
 export class LoadSurveysController implements Controller {
@@ -6,8 +7,8 @@ export class LoadSurveysController implements Controller {
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.loadSurveys.load()
+    const surveys = await this.loadSurveys.load()
 
-    return null
+    return ok(surveys)
   }
 }
