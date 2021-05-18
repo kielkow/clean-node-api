608ac9031bbc75d284fed2e2291a991b09fca480
kielkow <matheuskiel@fiorifer.com.br>
2021-05-18 17:57:48 -0300

    feat: ensure SaveSurveyController returns 403 if LoadSurveyById returns null

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
@@ -1,3 +1,5 @@
+import { InvalidParamError } from '@/presentation/errors'
+import { forbidden } from '@/presentation/helpers/http/http-helper'
 import {
   Controller,
   HttpRequest,
@@ -11,7 +13,12 @@ export class SaveSurveyResultController implements Controller {
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.loadSurveyById.loadById(httpRequest.params.surveyId)
+    const survey = await this.loadSurveyById.loadById(httpRequest.params.surveyId)
+
+    if (!survey) {
+      return forbidden(new InvalidParamError('surveyId'))
+    }
+
     return null
   }
 }
