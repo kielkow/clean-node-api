1943362917e1f4c8b0cfbced9c50a1a35e48ce19
kielkow <matheuskiel@fiorifer.com.br>
2021-06-07 09:47:01 -0300

    feat: add swagger api

--- src/main/config/config-swagger.ts
+++ src/main/config/config-swagger.ts
@@ -1,6 +1,7 @@
 import { Express } from 'express'
+import swaggerConfig from '@/main/docs'
 import { serve, setup } from 'swagger-ui-express'
 
 export default (app: Express): void => {
-  app.use('/api-docs', serve, setup())
+  app.use('/api-docs', serve, setup(swaggerConfig))
 }
---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Clean Node API',
+    description: 'An completed API with Typescript using TDD, Clean Architecture, Design Patterns and SOLID.',
+    version: '2.2.0'
+  }
+}
