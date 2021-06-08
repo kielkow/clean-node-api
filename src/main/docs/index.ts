c76c17a7334139d8b0e42b221aa8ce0a200e9913
kielkow <matheuskiel@fiorifer.com.br>
2021-06-08 12:05:49 -0300

    feat: start login api documentation

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,8 +1,29 @@
+import { loginPath } from './paths/login-path'
+import { accountSchema } from './schemas/account-schema'
+import { loginParamsSchema } from './schemas/login-params-schema'
+
 export default {
   openapi: '3.0.0',
   info: {
     title: 'Clean Node API',
     description: 'An completed API with Typescript using TDD, Clean Architecture, Design Patterns and SOLID.',
     version: '2.2.0'
+  },
+  servers: [
+    {
+      url: '/api'
+    }
+  ],
+  tags: [
+    {
+      name: 'Login'
+    }
+  ],
+  paths: {
+    '/login': loginPath
+  },
+  schemas: {
+    account: accountSchema,
+    loginParams: loginParamsSchema
   }
 }
---
+++ src/main/docs/paths/login-path.ts
@@ -0,0 +1,27 @@
+export const loginPath = {
+  post: {
+    tags: ['Login'],
+    summary: 'API to authenticate user',
+    requestBody: {
+      content: {
+        'application/json': {
+          schema: {
+            $ref: '#/schemas/loginParams'
+          }
+        }
+      }
+    },
+    responses: {
+      200: {
+        description: 'Sucess',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/account'
+            }
+          }
+        }
+      }
+    }
+  }
+}
