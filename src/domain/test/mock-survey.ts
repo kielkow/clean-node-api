1aeb62e8f8ed03a191a7b9832e195bdb56a34d69
kielkow <matheuskiel@fiorifer.com.br>
2021-07-05 16:22:24 -0300

    feat: ensure DbLoadSurveyResult returns empty survey result if there is no result

--- src/domain/test/mock-survey.ts
+++ src/domain/test/mock-survey.ts
@@ -5,10 +5,15 @@ export const mockSurveyModel = (): SurveyModel => {
   return {
     id: 'any_id',
     question: 'any_question',
-    answers: [{
-      image: 'any_image',
-      answer: 'any_answer'
-    }],
+    answers: [
+      {
+        answer: 'any_answer'
+      },
+      {
+        answer: 'other_answer',
+        image: 'any_image'
+      }
+    ],
     date: new Date()
   }
 }
