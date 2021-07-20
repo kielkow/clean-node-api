731b0eab1b9dae6fd76a582042b2c4425e55f2e8
kielkow <matheuskiel@fiorifer.com.br>
2021-07-19 22:16:01 -0300

    docs: add load survey result path

--- src/main/docs/paths/survey-result-path.ts
+++ src/main/docs/paths/survey-result-path.ts
@@ -46,5 +46,43 @@ export const surveyResultPath = {
         $ref: '#/components/serverError'
       }
     }
+  },
+  get: {
+    security: [{
+      apiKeyAuth: []
+    }],
+    tags: ['Survey'],
+    summary: 'API to load survey result',
+    description: 'This route can be executed by **authenticated users**',
+    parameters: [{
+      in: 'path',
+      name: 'surveyId',
+      description: 'survey ID',
+      required: true,
+      schema: {
+        type: 'string'
+      }
+    }],
+    responses: {
+      200: {
+        description: 'Sucess',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/surveyResult'
+            }
+          }
+        }
+      },
+      403: {
+        $ref: '#/components/forbidden'
+      },
+      404: {
+        $ref: '#/components/notFound'
+      },
+      500: {
+        $ref: '#/components/serverError'
+      }
+    }
   }
 }
