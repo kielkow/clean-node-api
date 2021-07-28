0c14538f06fd87280a8bfff8814ff22b016413d1
kielkow <matheuskiel@fiorifer.com.br>
2021-07-28 11:54:44 -0300

    feat: ensure LoadSurveyResultController is called with correct values

--- src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
@@ -25,7 +25,10 @@ export class LoadSurveyResultController implements Controller {
         return forbidden(new InvalidParamError('surveyId'))
       }
 
-      const surveyResult = await this.loadSurveyResult.load(surveyId)
+      const surveyResult = await this.loadSurveyResult.load(
+        surveyId,
+        httpRequest.accountId
+      )
 
       return ok(surveyResult)
     } catch (error) {
