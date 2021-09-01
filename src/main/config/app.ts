07e5d93183321d5ec23f4723e85ca4b85e8d0b7c
kielkow <matheuskiel@fiorifer.com.br>
2021-08-31 23:32:21 -0300

    feat: setup apollo server

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,7 +1,8 @@
 import express from 'express'
 
-import setupSwagger from './config-swagger'
+import setupSwagger from './swagger'
 import setupMiddlewares from './middlewares'
+import setupApolloServer from './apollo-server'
 import setupRoutes from './routes'
 import setupStaticFiles from './static-files'
 
@@ -11,5 +12,6 @@ setupStaticFiles(app)
 setupSwagger(app)
 setupMiddlewares(app)
 setupRoutes(app)
+setupApolloServer(app)
 
 export default app
--- src/main/config/swagger.ts
+++ src/main/config/swagger.ts
@@ -1,5 +1,6 @@
 import swaggerConfig from '@/main/docs'
 import { noCache } from '@/main/middlewares/no-cache'
+
 import { serve, setup } from 'swagger-ui-express'
 import { Express } from 'express'
 
