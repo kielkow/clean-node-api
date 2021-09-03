75586ec5df0d18938e87cad7f34ab8f57a42b232
kielkow <matheuskiel@fiorifer.com.br>
2021-09-03 12:52:14 -0300

    feat: add graphql survey schema

--- src/main/adapters/apollo-server-resolver-adapter.ts
+++ src/main/adapters/apollo-server-resolver-adapter.ts
@@ -9,9 +9,10 @@ import {
 
 export const adaptResolver = async (
   controller: Controller,
-  args: any
+  args?: any
 ): Promise<any> => {
-  const httpResponse = await controller.handle(args)
+  const request = { ...(args || {}) }
+  const httpResponse = await controller.handle(request)
 
   switch (httpResponse.statusCode) {
     case 200:
---
+++ src/main/graphql/resolvers/survey.ts
@@ -0,0 +1,11 @@
+import { adaptResolver } from '@/main/adapters/apollo-server-resolver-adapter'
+
+import {
+  makeLoadSurveysController
+} from '@/main/factories/controllers/survey/load-surveys/load-surveys-controller-factory'
+
+export default {
+  Query: {
+    surveys: async () => await adaptResolver(makeLoadSurveysController())
+  }
+}
