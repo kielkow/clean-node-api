db78d6335d4dd2ab2150f49fad1a481b1539e27b
kielkow <matheuskiel@fiorifer.com.br>
2021-09-18 16:28:52 -0300

    test: ensure SaveSurveyResult Query returns an survey result on success

--- tests/main/graphql/survey-result.test.ts
+++ tests/main/graphql/survey-result.test.ts
@@ -151,4 +151,74 @@ describe('SurveyResult GraphQL', () => {
       expect(res.errors[0].message).toBe('Access denied')
     })
   })
+
+  describe('SaveSurveyResult Mutation', () => {
+    const saveSurveyResultMutation = gql`
+      mutation saveSurveyResult ($surveyId: String!, $answer: String!) {
+        saveSurveyResult (surveyId: $surveyId, answer: $answer) {
+          question
+          answers {
+            answer
+            count
+            percent
+            isCurrentAccountAnswer
+          }
+          date
+        }
+      }
+    `
+
+    test('Should return SurveyResult', async () => {
+      const accessToken = await makeAccessToken()
+
+      const now = new Date()
+
+      const surveyRes = await surveyCollection.insertOne({
+        question: 'any_question',
+        answers: [
+          {
+            answer: 'any_answer',
+            image: 'http://image-name.com'
+          },
+          {
+            answer: 'other_answer'
+          }
+        ],
+        date: now
+      })
+
+      const { mutate } = createTestClient({
+        apolloServer,
+        extendMockRequest: {
+          headers: {
+            'x-access-token': accessToken
+          }
+        }
+      })
+
+      const res: any = await mutate(saveSurveyResultMutation, {
+        variables: {
+          surveyId: surveyRes.ops[0]._id.toString(),
+          answer: 'any_answer'
+        }
+      })
+
+      expect(res.data.saveSurveyResult.question).toBe('any_question')
+      expect(res.data.saveSurveyResult.date).toBe(now.toISOString())
+      expect(res.data.saveSurveyResult.answers).toEqual([
+        {
+          answer: 'any_answer',
+          count: 1,
+          percent: 100,
+          isCurrentAccountAnswer: true
+        },
+        {
+          answer: 'other_answer',
+          count: 0,
+          percent: 0,
+          isCurrentAccountAnswer: false
+        }
+      ])
+    })
+  })
 })
