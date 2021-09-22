030638a7050a43b0edd2273c3f9b9ddfbe667304
kielkow <matheuskiel@fiorifer.com.br>
2021-09-22 15:52:36 -0300

    refactor: implement join on routes config

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,12 +1,13 @@
 import { Express, Router } from 'express'
 import { readdirSync } from 'fs'
+import { join } from 'path'
 
 export default (app: Express): void => {
   const router = Router()
 
   app.use('/api', router)
 
-  readdirSync(`${__dirname}/../routes`).map(async file => {
+  readdirSync(join(__dirname, '../routes')).map(async file => {
     if (!file.includes('.test.') && !file.endsWith('.map')) {
       (await import(`../routes/${file}`)).default(router)
     }
